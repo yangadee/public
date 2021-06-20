48469226f9f0c6f13ed6ff5bf9290fea76319103
yangadee <msyangde@gmail.com>
2021-06-21 03:57:03 +0800

    update opencvrtsp.cpp

--- opencvrtsp.cpp
+++ opencvrtsp.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
+#include <string>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/videoio.hpp>
 
 int main(int argc, char **argv) {
 
-  cv::VideoCapture capture("rtsp://192.168.0.161:554/live2.sdp");
-
+//   cv::VideoCapture capture("rtsp://192.168.0.161:554/live2.sdp");
+//   std::string url = char* argv[1];
+  cv::VideoCapture capture(argv[1]);
   if (!capture.isOpened()) {
     // Error
     return 0;
@@ -25,10 +27,24 @@ int main(int argc, char **argv) {
     // std::cout << "open successful" << std ::endl;
     cv::imshow("TEST", frame);
 
-    cv::waitKey(30);
+    cv::waitKey(1);
   }
   return 0;
 }
-// TO complie
-// g++ rtspOpencv.cpp -o testoutput -std=c++11 `pkg-config --cflags --libs
-// opencv4`
+// To complie in mingw-64
+// g++ c:\Users\ADE\Desktop\vscode\opencvrtsp.cpp\
+// -o c:\Users\ADE\Desktop\vscode\opencvrtsp.exe\
+// -I C:\opencv\OpenCV-MinGW-Build\include\
+// -L C:\opencv\OpenCV-MinGW-Build\x64\mingw\bin \
+// -llibopencv_calib3d411 -llibopencv_core411\
+// -llibopencv_dnn411 -lopencv_features2d411\
+// -llibopencv_flann411 -llibopencv_highgui411\
+// -lopencv_imgcodecs411 -llibopencv_imgproc411\
+// -llibopencv_ml411 -llibopencv_objdetect411\
+// -llibopencv_photo411 -llibopencv_stitching411\
+// -llibopencv_video411 -llibopencv_videoio411 
+
+// to execute:
+// .\opencvrtsp.exe http://192.168.0.162/video1.mjpg
+// or use start-Process to run process in the background
+// start-Process .\opencvrtsp.exe http://192.168.0.162/video1.mjpg
