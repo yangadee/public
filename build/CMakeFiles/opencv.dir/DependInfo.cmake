581a6d3f56a81edd24088a9b45246a736a56f2e8
yangadee <msyangde@gmail.com>
2021-06-22 17:48:35 +0800

    update new implementation

---
+++ build/CMakeFiles/opencv.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/ADE/Desktop/vscode/opencvrtsp.cpp" "CMakeFiles/opencv.dir/opencvrtsp.cpp.obj" "gcc" "CMakeFiles/opencv.dir/opencvrtsp.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
